a346370f8eab9b6bcfd6b72fa212714ccb0c19cc
Ritvik3112 <73653627+Ritvik3112@users.noreply.github.com>
2021-01-19 09:34:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
+var mouseEvent = ["empty"]
 canvas = document.getElementById("my_Canvas");
 ctx = canvas.getContext("2d");
 
-function draw(){
-ctx.beginpath();
-ctx.strokeStyle = red;
-ctx.linewidth = 2;
-ctx.rect(150 , 143 , 430 , 200);
-ctx.stroke();
+color = "black";
+width_of_the_line = 1;
+radius = 5;
 
-ctx.beginpath();
-ctx.strokeStyle = blue;
-ctx.linewidth = 2;
-ctx.rect(200 , 143 , 430 , 200);
-ctx.stroke();
+canvas.addEventListener("mousedown" , my_mousedown);
 
-ctx.beginpath();
-ctx.strokeStyle = yellow;
-ctx.linewidth = 2;
-ctx.rect(250 , 143 , 430 , 200);
-ctx.stroke();
+function my_mousedown(e){
+     document.getElementById("input_col").value;
+     document.getElementById("width_of_the_line").value;
+     document.getElementById("radius").value;
+     mouseEvent = "mousedown";
+}
+canvas.addEventListener("mousemove" , my_mousemove);
 
-ctx.beginpath();
-ctx.strokeStyle = green;
-ctx.linewidth = 2;
-ctx.rect(300 , 143 , 430 , 200);
-ctx.stroke();
+function my_mousemove(e){
+    current_position_of_x = e.clientX - canvas.offsetLeft;
+    current_position_of_y = e.clientY - canvas.offsetTop;
 
-ctx.beginpath();
-ctx.strokeStyle = pink;
-ctx.linewidth = 2;
-ctx.rect(350 , 143 , 430 , 200);
-ctx.stroke();
+    if(mouseEvent == "moousedown"){
+        console.log("Current position of x and y coordinates = ")
+        console.log("x = " + current_position_of_x , "y = " + current_position_of_y);
+
+      ctx.beginpath();
+      ctx.strokeStyle = red;
+      ctx.linewidth = 2;
+      ctx.rect(current_position_of_x , current_position_of_y , radius , 0 , 2 * Math.PI);
+      ctx.stroke();
+    }
+}
+canvas.addEventListener("mouseUp" , my_mouseUp);
+
+function my_mouseUp(e){
+      mouseEvent = "mouseUp";
+}
+canvas.addEventListener("mouseleave" , my_mouseleave);
+
+function my_mouseleave(e){
+    mouseEvent = "mouseleave";
 }
 
-function clear(){
-  ctx.clearRect(0 , 0 , canvas.height , canvas.width);
-}
