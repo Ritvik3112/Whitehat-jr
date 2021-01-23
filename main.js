7f89826eabc7973282d7b9f69646bf46fc9ed726
Ritvik3112 <73653627+Ritvik3112@users.noreply.github.com>
2021-01-23 11:57:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,59 +1,79 @@
 canvas = document.getElementById("my_Canvas");
 ctx = canvas.getContext("2d");
 
-color = "black";
-width_of_the_line = 1;
+car1 = "car1.png";
+car1_x = 120;
+car1_y = 70;
+car1_width = 10;
+car1_height = 10;
 
-canvas.addEventListener("mousedown" , my_mousedown);
+car2 = "car1.png";
+car2_x = 120;
+car2_y = 70;
+car2_width = 10;
+car2_height = 10;
 
-function my_mousedown(e){
-     document.getElementById("input_col").value;
-     document.getElementById("width_of_the_line").value;
-     mouseEvent = "mousedown";
+background_img = "racin.jpg"
+
+
+function add(){
+    background_imgTag = new Image();
+    background_imgTag.onload = uploadbackground;
+    background_imgTag.src = background_img;
+
+    car1_imgTag = new Image();
+    car1_imgTag.onload = uploadcar1;
+    car1_imgTag.src = car1;
+
+    car2_imgTag = new Image();
+    car2_imgTag.onload = uploadrover;
+    car2_imgTag.src = car2;
 }
-canvas.addEventListener("mouseUp" , my_mouseUp);
-
-function my_mouseUp(e){
-      mouseEvent = "mouseUp";
+function uploadbackground(){
+    ctx.drawImage(background_imgTag , 0 , 0 , canvas.width , canvas.height);
 }
-canvas.addEventListener("mouseleave" , my_mouseleave);
-
-function my_mouseleave(e){
-    mouseEvent = "mouseleave";
+function uploadcar1(){
+    ctx.drawImage(car1_imgTag , car1_x , car1_y , car1_width , car1_height);
 }
-canvas.addEventListener("mousemove" , my_mousemove);
+function uploadcar2(){
+    ctx.drawImage(car2_imgTag , car2_x , car2_y , car2_width , car2_height);
+}
+window.addEventListener("keydown" , my_keydown);
 
-    function my_mousemove(e){
-        current_position_of_x = e.clientX - canvas.offsetLeft;
-        current_position_of_y = e.clientY - canvas.offsetTop;
-    
-        if(mouseEvent == "mousedown"){
-            console.log("Current position of x and y coordinates = ")
-            console.log("x = " + current_position_of_x , "y = " + current_position_of_y);
-    
-          ctx.beginpath();
-          ctx.strokeStyle = red;
-          ctx.linewidth = 2;
-          ctx.rect(current_position_of_x , current_position_of_y , radius , 0 , 2 * Math.PI);
-          ctx.stroke();
-
-          last_position_of_x = current_position_of_mouse_x;
-          last_position_of_y = current_position_of_mouse_y;
-        }
+function my_keydown(e){
+      keypressed = e.keycode;
+      console.log(keypressed);
+    if(keypressed == 38){
+        car1up();
+          console.log("up arrow key");
+      }
+    if(keypressed == 40){
+        car1down();
+        console.log("down arrow key");
     }
-    canvas.addEventListener("touchmove" , my_touchmove);
-
-    function my_touchmove(e){
-        current_position_of_x = e.clientX[0] - canvas.offsetLeft;
-        current_position_of_y = e.clientY[0] - canvas.offsetTop;
-       
-          ctx.beginpath();
-          ctx.strokeStyle = red;
-          ctx.linewidth = 2;
-          ctx.rect(current_position_of_x , current_position_of_y , radius , 0 , 2 * Math.PI);
-          ctx.stroke();
-
-          last_position_of_touch_x = current_position_of_touch_x;
-          last_position_of_y = current_position_of_touch_y;
+    if(keypressed == 39){
+        car1left();
+        console.log("left arrow key");
     }
+    if(keypressed == 37){
+        car1right();
+        console.log("right arrow key");
+    }
+    if(keypressed == 87){
+        car1up();
+          console.log("key w");
+      }
+    if(keypressed == 65){
+        car1down();
+        console.log("key a");
+    }
+    if(keypressed == 83){
+        car1left();
+        console.log("key s");
+    }
+    if(keypressed == 68){
+        car1right();
+        console.log("key d");
+    }
+}
 
